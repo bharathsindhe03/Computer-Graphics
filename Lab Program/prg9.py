231648594a808c410e9f9761acb62289c73dfea9
bharathsindhe03 <sindhebharath10@gmail.com>
2024-08-01 18:14:58 +0530

    prg7-12

--- Lab Program/prg9.py
+++ Lab Program/prg9.py
@@ -2,21 +2,17 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-# Load the image 
-image_path = "logo.jpeg"  # Replace with the path to your image 
+
+image_path = "logo.jpeg" 
 img = cv2.imread(image_path) 
 
-# Convert the image to grayscale 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-# Edge detection 
-edges = cv2.Canny(gray, 100, 200)  # Use Canny edge detector 
+edges = cv2.Canny(gray, 100, 200) 
 
-# Texture extraction 
-kernel = np.ones((5, 5), np.float32) / 25  # Define a 5x5 averaging kernel 
-texture = cv2.filter2D(gray, -1, kernel)  # Apply the averaging filter for texture extraction 
+kernel = np.ones((5, 5), np.float32) / 25  
+texture = cv2.filter2D(gray, -1, kernel)  
 
-# Display the original image, edges, and texture using matplotlib 
 plt.figure(figsize=(10, 5)) 
 plt.subplot(1, 3, 1) 
 plt.title("Original Image") 
